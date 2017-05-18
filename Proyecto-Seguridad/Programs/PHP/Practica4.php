4781a6e64e7844008cc6250d928b23ad201be626
Alexdrowned <warriorofwars@hotmail.com>
2017-05-18 02:19:06 -0500

    Avances, registro terminado, practicas algo asi, un poco de diseño y listo

--- Proyecto-Seguridad/Programs/PHP/Practica4.php
+++ Proyecto-Seguridad/Programs/PHP/Practica4.php
@@ -1,7 +1,14 @@
 <?php
-$no_cta="316030120";
-echo $no_cta."<br/>";
-$rev=strrev($no_cta);
-echo $rev ;
+echo "<!Doctype html>
+			<html>
+				<head>
+				</head>
+				<body>";
+				$no_cta="316030120";
+				echo $no_cta."<br/>";
+				$rev=strrev($no_cta);
+				echo $rev ;
+		echo    "</body>
+	  	  	</html>";
 
 ?>
---
+++ Proyecto-Seguridad/Programs/PHP/validaCurp.php
@@ -0,0 +1,18 @@
+<?php
+echo "<!Doctype html>
+			<html>
+				<head>
+				</head>
+				<body>";
+				$curp="/^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/";
+				$validar="BABA000304HDFTLXA4";
+				echo "Curp a validar: ".$validar."<br/>";
+				if (preg_match($curp, $validar)) 
+            		echo "Curp valida";
+        		else 
+        			echo "Curp no valida";
+        echo "</body>
+	  	  	</html>";
+        
+
+?>
